960b3c696052413cf49b8773c472b826efd62310
geleeroyale <krassvs@gmail.com>
2019-10-10 17:12:32 +0200

    init

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Interspace.chat v.0</h1>
+      </header>
+      <body>
+        <div className="container">
+          <div className="video"></div>
+          <div className="click-zone a" data-zone="Living Room"></div>
+          <div className="click-zone b" data-zone="Dining Room"></div>
+          <div className="click-zone c" data-zone="Sunset Patio"></div>
+          <div className="click-zone hexagon" data-zone="Portal Zone"></div>
+          <img
+            className="click-zone pavle"
+            id="pavle"
+            src="pavle.png"
+            data-zone="Pavle"
+          />
+        </div>
+        <div className="container2">
+          <p>
+            You're in the <span id="roomTitle">Graveyard</span>, Fucka!
+          </p>
+        </div>
+      </body>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Click.js
@@ -0,0 +1,50 @@
+import React from "react";
+
+var offsetLeft = 0;
+
+function Click(e) {
+  // alert('Clicked: ' + this.dataset.zone);
+  document.getElementById("roomTitle").innerHTML = this.dataset.zone;
+
+  // offsetLeft += 20;
+  //
+  // pavle.style.position = "relative";
+  // pavle.style.left = (offsetLeft)+"px";
+  // e.preventDefault();     //prevents the page from redirecting
+
+  if (this.dataset.zone == "Living Room") {
+    console.log("works");
+    let pavle = document.getElementsByClassName("pavle");
+    document.getElementById("pavle").style.top = 75 + "px";
+    document.getElementById("pavle").style.left = 200 + "px";
+  } else if (this.dataset.zone == "Dining Room") {
+    console.log("works");
+    let pavle = document.getElementsByClassName("pavle");
+    document.getElementById("pavle").style.top = 75 + "px";
+    document.getElementById("pavle").style.left = 300 + "px";
+  } else if (this.dataset.zone == "Sunset Patio") {
+    console.log("works");
+    let pavle = document.getElementsByClassName("pavle");
+    document.getElementById("pavle").style.top = 200 + "px";
+    document.getElementById("pavle").style.left = 300 + "px";
+  } else if (this.dataset.zone == "Portal Zone") {
+    console.log("works");
+    let pavle = document.getElementsByClassName("pavle");
+    document.getElementById("pavle").style.top = 200 + "px";
+    document.getElementById("pavle").style.left = 420 + "px";
+  } else {
+    console.log("works");
+    // var pavle = document.getElementsByClassName('pavle');
+    // document.getElementById('pavle').style.top = 0+"px";
+    // document.getElementById('pavle').style.left = 0+"px";
+  }
+
+  // document.getElementById("pavle").style.margintop=400+"px";
+}
+
+var clickZones = document.getElementsByClassName("click-zone");
+for (var i = 0; i < clickZones.length; i += 1) {
+  clickZones[i].addEventListener("Click", Click);
+}
+
+export default Click;
