4ae937fd6e0713ae0e194cc2a11e3feb52ca731e
geleeroyale <krassvs@gmail.com>
2020-03-11 13:43:18 +0100

    switch jitsi-meet from official to own deployment

--- src/components/JitsiInstance.js
+++ src/components/JitsiInstance.js
@@ -1,28 +1,27 @@
-import React, { useEffect } from "react";
+import React, { useEffect } from 'react';
 // import * as JitsiMeetExternalAPI from "../dist/jitsi";
 
 const JitsiMeetExternalAPI = window.JitsiMeetExternalAPI;
 
-const JitsiInstance = ({width, height, space}) => {
+const JitsiInstance = ({ width, height, space }) => {
+	useEffect(() => {
+		const domain = 'interspace.geleeroyale.work/metagame';
+		const options = {
+			roomName: space,
+			width: width,
+			height: height,
+			parentNode: document.querySelector('#meet')
+		};
+		const api = new JitsiMeetExternalAPI(domain, options);
 
-  useEffect(() => {
-    const domain = "meet.jit.si/interspace-metagame";
-    const options = {
-      roomName: space,
-      width: width,
-      height: height,
-      parentNode: document.querySelector("#meet")
-    };
-    const api = new JitsiMeetExternalAPI(domain, options);
+		// api.executeCommand("displayName", user);
 
-    // api.executeCommand("displayName", user);
+		return function cleanup() {
+			api.dispose();
+		};
+	}, [space, width, height]);
 
-    return function cleanup() {
-      api.dispose();
-    };
-  }, [space, width, height]);
-
-  return <div id="meet"></div>;
+	return <div id='meet'></div>;
 };
 
 export default JitsiInstance;
