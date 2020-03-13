029c94e8fa5326d0d506abb53d802db19f021070
geleeroyale <krassvs@gmail.com>
2020-03-13 11:00:23 +0100

    change jitsi instance to self deployed

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,29 +1,46 @@
 export const RoomURLs = {
-    "House of Defiance": {
-        jitsi: { domain: 'meet.jit.si/interspace-metagame', roomName: 'House of Defiance' },
-        mozillaHub: { externalUrl: 'https://hubs.mozilla.com/nKCuNrg/house-of-defiance' },
-        youtube: { videoId: 'X5k8Cbr0d44' },
-    },
-    "House of DAOs": {
-        jitsi: { domain: 'meet.jit.si/interspace-metagame', roomName: 'House of DAOs' },
-        mozillaHub: { externalUrl: 'https://hubs.mozilla.com/eJZGNU5/house-of-daos' },
-        youtube: { videoId: 'k0UpUwmKaHc' },
-    },
-    "House of Adoption": {
-        jitsi: { domain: 'meet.jit.si/interspace-metagame', roomName: 'House of Adoption' },
-        mozillaHub: { externalUrl: 'https://hubs.mozilla.com/tpKKcfA/house-of-adoption' },
-        youtube: { videoId: '_DxQQKrxYFI' },
-    },
-    "Stress Test Arena": {
-        jitsi: { domain: 'meet.jit.si/interspace-metagame', roomName: 'Stress Test Arena' },
-        mozillaHub: { externalUrl: 'https://hubs.mozilla.com/nx5rV57/stress-test-arena' },
-        youtube: { videoId: 'pWfUAjIgoJM' },
-    },
-    "Raid Guild": {
-        jitsi: { domain: 'meet.jit.si/interspace-metagame', roomName: 'Raid Guild' },
-        mozillaHub: { externalUrl: 'https://hubs.mozilla.com/LmrJQqL/raid-guild' },
-        youtube: { videoId: 'f2bnDe5-3mM' },
-    },
+	'House of Defiance': {
+		jitsi: {
+			domain: 'interspace.geleeroyale.work',
+			roomName: 'House of Defiance'
+		},
+		mozillaHub: {
+			externalUrl: 'https://hubs.mozilla.com/nKCuNrg/house-of-defiance'
+		},
+		youtube: { videoId: 'X5k8Cbr0d44' }
+	},
+	'House of DAOs': {
+		jitsi: { domain: 'interspace.geleeroyale.work', roomName: 'House of DAOs' },
+		mozillaHub: {
+			externalUrl: 'https://hubs.mozilla.com/eJZGNU5/house-of-daos'
+		},
+		youtube: { videoId: 'k0UpUwmKaHc' }
+	},
+	'House of Adoption': {
+		jitsi: {
+			domain: 'interspace.geleeroyale.work',
+			roomName: 'House of Adoption'
+		},
+		mozillaHub: {
+			externalUrl: 'https://hubs.mozilla.com/tpKKcfA/house-of-adoption'
+		},
+		youtube: { videoId: '_DxQQKrxYFI' }
+	},
+	'Stress Test Arena': {
+		jitsi: {
+			domain: 'interspace.geleeroyale.work',
+			roomName: 'Stress Test Arena'
+		},
+		mozillaHub: {
+			externalUrl: 'https://hubs.mozilla.com/nx5rV57/stress-test-arena'
+		},
+		youtube: { videoId: 'pWfUAjIgoJM' }
+	},
+	'Raid Guild': {
+		jitsi: { domain: 'interspace.geleeroyale.work', roomName: 'Raid Guild' },
+		mozillaHub: { externalUrl: 'https://hubs.mozilla.com/LmrJQqL/raid-guild' },
+		youtube: { videoId: 'f2bnDe5-3mM' }
+	}
 };
 
 export const RoomNames = Object.keys(RoomURLs);
