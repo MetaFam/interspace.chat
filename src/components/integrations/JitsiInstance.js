dadf882c696b12ba5d63b995457b61cc5cde6344
geleeroyale <krassvs@gmail.com>
2020-03-13 11:00:10 +0100

    bring back deleted file from merge

---
+++ src/components/integrations/JitsiInstance.js
@@ -0,0 +1,26 @@
+import React, { useEffect } from 'react';
+// import * as JitsiMeetExternalAPI from "../dist/jitsi";
+
+const JitsiMeetExternalAPI = window.JitsiMeetExternalAPI;
+
+const JitsiInstance = ({ width, height, roomData }) => {
+	useEffect(() => {
+		const options = {
+			roomName: roomData.roomName,
+			width: width,
+			height: height,
+			parentNode: document.querySelector('#meet')
+		};
+		const api = new JitsiMeetExternalAPI(roomData.domain, options);
+
+		// api.executeCommand("displayName", user);
+
+		return function cleanup() {
+			api.dispose();
+		};
+	}, [roomData, width, height]);
+
+	return <div id='meet'></div>;
+};
+
+export default JitsiInstance;
