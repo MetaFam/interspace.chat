fbedf6656db30145eca9afd6c3a7eff501c753ed
Mulenga Bowa <mulengabowa53@gmail.com>
2020-04-10 14:54:13 +0200

    added mobile support

--- src/components/Space.js
+++ src/components/Space.js
@@ -1,5 +1,7 @@
 import React, { Fragment, useContext } from 'react';
 import styled from 'styled-components';
+import { TransformWrapper, TransformComponent } from 'react-zoom-pan-pinch';
+import { isMobile } from 'react-device-detect';
 
 import { FloatingSpaceContext } from "../contexts/FloatingSpaceContext";
 
@@ -27,9 +29,8 @@ const CurrentSpace = styled.span`
 
 const Disclaimer = styled.div`
 	position: relative;
-	top: 830px;
 	margin-bottom: 25px;
-	background: unset;
+  background: unset;
 `;
 
 
@@ -87,66 +88,76 @@ const Space = () => {
           Click a location to join a conversation.
         </span>
         */}
-        <img src="metaspace.png" className="image-map" alt="map" />
-        <div
-          className="click-zone a"
-          onClick={() => addFloatingSpace("House of Defiance")}
+        <TransformWrapper
+          defaultScale={isMobile ? 0.6 : 1}
+          options={{
+            minScale: 0.5,
+            maxScale: 1.5
+          }}
         >
-          <span className="roomName">House of Defiance</span>
-          <div className="click-zone-highlight a"></div>
-        </div>
-        <div
-          className="click-zone b"
-          onClick={() => addFloatingSpace("House of DAOs")}
-        >
-          <span className="roomName">House of DAOs</span>
-          <div className="click-zone-highlight b"></div>
-        </div>
-        <div
-          className="click-zone c"
-          onClick={() => addFloatingSpace("Raid Guild")}
-        >
-          <span className="roomName">Raid Guild</span>
-          <div className="click-zone-highlight c"></div>
-        </div>
-        <div
-          className="click-zone d"
-          data-zone="stress-test-arena"
-          onClick={() => addFloatingSpace("Stress Test Arena")}
-        >
-          <span className="roomName" style={portalStyle}>
-            Stress Test Arena
-          </span>
-          <div className="click-zone-highlight d"></div>
-        </div>
-        <div
-          className="click-zone e"
-          onClick={() => addFloatingSpace("House of Adoption")}
-        >
-          <span className="roomName">House of Adoption</span>
-          <div className="click-zone-highlight e"></div>
-        </div>
-        <div
-          className="click-zone f"
-          onClick={() => addFloatingSpace("loft.radio")}
-        >
-          <span className="roomName">loft.radio</span>
-          <div className="click-zone-highlight f"></div>
-        </div>
-        <div
-          className="click-zone g"
-          onClick={() => addFloatingSpace("rTrees")}
-        >
-          <span className="roomName">rTrees</span>
-          <div className="click-zone-highlight g"></div>
-        </div>
-        <div
-          className='click-zone h'
-          onClick={() => openInNewTab('https://forum.metagame.wtf/')}
-        >
-          <span className='roomName'>Forum</span>
-          <div className='click-zone-highlight h'></div>
-        </div>
+          <TransformComponent>
+            <img src="metaspace.png" className="image-map" alt="map" />
+            <div
+              className="click-zone a"
+              onClick={() => addFloatingSpace("House of Defiance")}
+            >
+              <span className="roomName">House of Defiance</span>
+              <div className="click-zone-highlight a"></div>
+            </div>
+            <div
+              className="click-zone b"
+              onClick={() => addFloatingSpace("House of DAOs")}
+            >
+              <span className="roomName">House of DAOs</span>
+              <div className="click-zone-highlight b"></div>
+            </div>
+            <div
+              className="click-zone c"
+              onClick={() => addFloatingSpace("Raid Guild")}
+            >
+              <span className="roomName">Raid Guild</span>
+              <div className="click-zone-highlight c"></div>
+            </div>
+            <div
+              className="click-zone d"
+              data-zone="stress-test-arena"
+              onClick={() => addFloatingSpace("Stress Test Arena")}
+            >
+              <span className="roomName" style={portalStyle}>
+                Stress Test Arena
+              </span>
+              <div className="click-zone-highlight d"></div>
+            </div>
+            <div
+              className="click-zone e"
+              onClick={() => addFloatingSpace("House of Adoption")}
+            >
+              <span className="roomName">House of Adoption</span>
+              <div className="click-zone-highlight e"></div>
+            </div>
+            <div
+              className="click-zone f"
+              onClick={() => addFloatingSpace("loft.radio")}
+            >
+              <span className="roomName">loft.radio</span>
+              <div className="click-zone-highlight f"></div>
+            </div>
+            <div
+              className="click-zone g"
+              onClick={() => addFloatingSpace("rTrees")}
+            >
+              <span className="roomName">rTrees</span>
+              <div className="click-zone-highlight g"></div>
+            </div>
+            <div
+              className='click-zone h'
+              onClick={() => openInNewTab('https://forum.metagame.wtf/')}
+            >
+              <span className='roomName'>Forum</span>
+              <div className='click-zone-highlight h'></div>
+            </div>
+          </TransformComponent>
+        </TransformWrapper>
       </div>
       <Disclaimer>
         Feel free to make improvements to the map, download the .psd file{" "}
