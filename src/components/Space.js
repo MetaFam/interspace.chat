baba2c0e649778e7825f34bdd15cc8bb15a341cd
Manolingam <manolingam1996@gmail.com>
2020-04-21 13:44:46 +0530

    updated a lot of styles in the process of making the page responsive

--- src/components/Space.js
+++ src/components/Space.js
@@ -32,107 +32,71 @@ const Space = () => {
 	}
 
 	return (
-		<div>
-			<span style={{ marginBottom: '0px' }}>
-				<div
-					style={{
-						position: 'absolute',
-						top: '10px',
-						left: 0,
-						right: 0,
-						margin: 'auto',
-					}}
+		<div className='map-container'>
+			<div style={{ zIndex: 1, marginBottom: '1rem' }}>
+				Welcome to{' '}
+				<a
+					href='https://metagame.wtf'
+					target='_blank'
+					rel='noopener noreferrer'
 				>
-					Welcome to{' '}
-					<a
-						href='https://metagame.wtf'
-						target='_blank'
-						rel='noopener noreferrer'
-					>
-						MetaGame
-					</a>
-					<i style={{ fontSize: '0.6em' }}> v0.1</i>
-				</div>
-				<div>
-					{displayedJoinedSpaces && (
-						<Fragment>
-							You're in the <div>{displayedJoinedSpaces}</div>!
-						</Fragment>
-					)}
-				</div>
-			</span>
-
-			<div className='map-container'>
+					MetaGame
+				</a>
+				<i style={{ fontSize: '0.6em' }}> v0.1</i>
+			</div>
+			<div className='octo-container'>
 				<img src='metaspace.png' className='image-map' alt='map' />
 				<div
 					className='click-zone a'
 					onClick={() => addFloatingSpace('House of Defiance')}
 				>
-					<span className='roomName'>House of Defiance</span>
-					{/* <div className='click-zone-highlight a'></div> */}
+					House of Defiance
 				</div>
 				<div
 					className='click-zone b'
 					onClick={() => addFloatingSpace('House of DAOs')}
 				>
-					<span className='roomName'>House of DAOs</span>
-					{/* <div className='click-zone-highlight b'></div> */}
+					House of DAOs
 				</div>
 				<div
 					className='click-zone c'
 					onClick={() => addFloatingSpace('Raid Guild')}
 				>
-					<span className='roomName'>Raid Guild</span>
-					{/* <div className='click-zone-highlight c'></div> */}
+					Raid Guild
 				</div>
 				<div
 					className='click-zone d'
 					data-zone='stress-test-arena'
 					onClick={() => addFloatingSpace('Stress Test Arena')}
 				>
-					<span className='roomName' style={portalStyle}>
-						Stress Test Arena
-					</span>
-					{/* <div className='click-zone-highlight d'></div> */}
+					Stress Test Arena
 				</div>
 				<div
 					className='click-zone e'
 					onClick={() => addFloatingSpace('House of Adoption')}
 				>
-					<span className='roomName'>House of Adoption</span>
-					{/* <div className='click-zone-highlight e'></div> */}
+					House of Adoption
 				</div>
 				<div
 					className='click-zone f'
 					onClick={() => addFloatingSpace('loft.radio')}
 				>
-					<span className='roomName'>loft.radio</span>
-					{/* <div className='click-zone-highlight f'></div> */}
+					loft.radio
 				</div>
 				<div
 					className='click-zone g'
 					onClick={() => addFloatingSpace('rTrees')}
 				>
-					<span className='roomName'>rTrees</span>
-					{/* <div className='click-zone-highlight g'></div> */}
+					rTrees
 				</div>
 				<div
 					className='click-zone h'
 					onClick={() => openInNewTab('https://forum.metagame.wtf/')}
 				>
-					<span className='roomName'>Forum</span>
-					{/* <div className='click-zone-highlight h'></div> */}
+					Forum
 				</div>
 			</div>
-			<div
-				style={{
-					position: 'absolute',
-					bottom: '10px',
-					left: 0,
-					right: 0,
-					margin: 'auto',
-				}}
-			>
+			<div style={{ zIndex: 1 }}>
 				Feel free to make improvements to the map, download the .psd
 				file{' '}
 				<a href='https://www.dropbox.com/s/cocwaannzy8lqty/Interspace%20v0.2.psd?dl=0'>
