c129c12c9d53959881514e98b610d44d3c070ba9
geleeroyale <admin@geleeroyale.work>
2021-02-13 23:48:08 +0100

    make it nice for mobile (part2)

--- src/components/announcement.js
+++ src/components/announcement.js
@@ -1,5 +1,6 @@
 import React, { Fragment, useContext } from 'react'
 import styled from 'styled-components'
+import { useMediaQuery } from 'react-responsive'
 
 import HeaderImage from '../img/metafest-header.png'
 import Date from '../img/metafest-date.png'
@@ -10,133 +11,238 @@ import Noise from '../img/metafest-noise.gif'
 import Rocks from '../img/metafest-rocks.gif'
 import Wanderer from '../img/metafest-wanderer.png'
 
-const StyledHeadline = styled.h1`
-  font-size: 4rem;
-  color: #f900a9;
-  position: relative;
-`
-const Glow = styled.h1`
-  font-size: 4rem;
-  color: #f900a9;
-  filter: blur(26px);
-  opacity: 0.69;
-  position: relative;
-  top: -7rem;
-  border-radius: 20px;
-`
-
 const StyledLink = styled.a`
   color: #e340a8;
   text-align: center;
 `
 
 const Announce = () => {
+  const isPortrait = useMediaQuery({ query: '(orientation: portrait)' })
+  const isMobile = useMediaQuery({ query: '(max-width: 1224px)' })
   return (
-    <div
-      style={{
-        width: '100vw',
-        height: '100vh',
-        overflowY: 'auto',
-        display: 'grid',
-        placeItems: 'center'
-      }}
-    >
-      <img
-        src={Date}
-        alt='March 7th - April 4th'
-        width='30%'
-        style={{
-          position: 'fixed',
-          marginLeft: 'auto',
-          marginRight: 'auto',
-          left: '0',
-          right: '0'
-        }}
-      />
-      <img
-        src={HeaderImage}
-        alt='MetaFest'
-        width='60%'
-        style={{
-          position: 'fixed',
-          marginLeft: 'auto',
-          marginRight: 'auto',
-          left: '0',
-          right: '0',
-          top: '-3rem'
-        }}
-      />
-      <div>
-        <img
-          src={Flavortext}
-          alt='a shitload of fun'
-          width='60%'
+    <React.Fragment>
+      {isPortrait && isMobile ? (
+        <div
           style={{
-            position: 'absolute',
-            top: '60%',
-            right: '0%',
-            zIndex: '1',
-            width: '30%'
+            width: '100vw',
+            height: '100vh',
+            overflowY: 'auto',
+            display: 'grid',
+            placeItems: 'center'
           }}
-        />
-      </div>
-      <div
-        style={{
-          position: 'absolute',
-          zIndex: '100',
-          left: '0%',
-          right: '0%',
-          marginLeft: 'auto',
-          marginRight: 'auto',
-          bottom: '10vh',
-          backgroundColor: '#000000AA',
-          padding: '1rem'
-        }}
-      >
-        <h2 style={{ color: '#fff' }}>Fill out an application to join</h2>
-        <StyledLink href='http://bit.ly/MetaFestMods'>MOD</StyledLink>
-        {'   '}
-        <StyledLink href='http://bit.ly/MetaFestSpeakers'>SPEAKER</StyledLink>
-        {'   '}
-        <StyledLink href='http://bit.ly/MetaFestMG'>METAFAM</StyledLink>
-      </div>
-      <img
-        src={Wanderer}
-        alt=''
-        width='50%'
-        style={{
-          position: 'absolute',
-          bottom: '0%',
-          left: '0%',
-          zIndex: '10'
-        }}
-      />
-      <img
-        src={Rocks}
-        alt=''
-        width='100%'
-        style={{
-          position: 'fixed',
-          marginLeft: 'auto',
-          marginRight: 'auto',
-          left: '0',
-          right: '0',
-          bottom: '0',
-          zIndex: '1'
-        }}
-      />
-      <img
-        src={Grid}
-        alt=''
-        width='100%'
-        height='60%'
-        style={{
-          position: 'fixed',
-          bottom: '0',
-          zIndex: '0'
-        }}
-      />
-    </div>
+        >
+          <img
+            src={Date}
+            alt='March 7th - April 4th'
+            width='30%'
+            style={{
+              position: 'fixed',
+              marginLeft: 'auto',
+              marginRight: 'auto',
+              left: '0',
+              right: '0',
+              top: '7rem'
+            }}
+          />
+          <img
+            src={HeaderImage}
+            alt='MetaFest'
+            width='60%'
+            style={{
+              position: 'fixed',
+              marginLeft: 'auto',
+              marginRight: 'auto',
+              left: '0',
+              right: '0',
+              top: '4rem'
+            }}
+          />
+          <div>
+            <img
+              src={Flavortext}
+              alt='a shitload of fun'
+              width='60%'
+              style={{
+                position: 'absolute',
+                marginLeft: 'auto',
+                marginRight: 'auto',
+                left: '0',
+                right: '0',
+                top: '25vh',
+                zIndex: '1',
+                width: '30%'
+              }}
+            />
+          </div>
+          <div
+            style={{
+              position: 'absolute',
+              zIndex: '100',
+              left: '0%',
+              right: '0%',
+              marginLeft: 'auto',
+              marginRight: 'auto',
+              bottom: '40vh',
+              backgroundColor: '#000000AA',
+              padding: '1rem'
+            }}
+          >
+            <h2 style={{ color: '#fff' }}>Fill out an application to join</h2>
+            <StyledLink href='http://bit.ly/MetaFestMods'>MOD</StyledLink>
+            {'   '}
+            <StyledLink href='http://bit.ly/MetaFestSpeakers'>
+              SPEAKER
+            </StyledLink>
+            {'   '}
+            <StyledLink href='http://bit.ly/MetaFestMG'>METAFAM</StyledLink>
+          </div>
+          <img
+            src={Wanderer}
+            alt=''
+            width='50%'
+            style={{
+              position: 'absolute',
+              bottom: '0%',
+              left: '0%',
+              zIndex: '10'
+            }}
+          />
+          <img
+            src={Rocks}
+            alt=''
+            width='100%'
+            style={{
+              position: 'fixed',
+              marginLeft: 'auto',
+              marginRight: 'auto',
+              left: '0',
+              right: '0',
+              bottom: '0',
+              zIndex: '1'
+            }}
+          />
+          <img
+            src={Grid}
+            alt=''
+            width='100%'
+            height='60%'
+            style={{
+              position: 'fixed',
+              bottom: '0',
+              zIndex: '0'
+            }}
+          />
+        </div>
+      ) : (
+        <div
+          style={{
+            width: '100vw',
+            height: '100vh',
+            overflowY: 'auto',
+            display: 'grid',
+            placeItems: 'center'
+          }}
+        >
+          <img
+            src={Date}
+            alt='March 7th - April 4th'
+            width='30%'
+            style={{
+              position: 'fixed',
+              marginLeft: 'auto',
+              marginRight: 'auto',
+              left: '0',
+              right: '0'
+            }}
+          />
+          <img
+            src={HeaderImage}
+            alt='MetaFest'
+            width='60%'
+            style={{
+              position: 'fixed',
+              marginLeft: 'auto',
+              marginRight: 'auto',
+              left: '0',
+              right: '0',
+              top: '-3rem'
+            }}
+          />
+          <div>
+            <img
+              src={Flavortext}
+              alt='a shitload of fun'
+              width='60%'
+              style={{
+                position: 'absolute',
+                top: '60%',
+                right: '0%',
+                zIndex: '1',
+                width: '30%'
+              }}
+            />
+          </div>
+          <div
+            style={{
+              position: 'absolute',
+              zIndex: '100',
+              left: '0%',
+              right: '0%',
+              marginLeft: 'auto',
+              marginRight: 'auto',
+              bottom: '10vh',
+              backgroundColor: '#000000AA',
+              padding: '1rem'
+            }}
+          >
+            <h2 style={{ color: '#fff' }}>Fill out an application to join</h2>
+            <StyledLink href='http://bit.ly/MetaFestMods'>MOD</StyledLink>
+            {'   '}
+            <StyledLink href='http://bit.ly/MetaFestSpeakers'>
+              SPEAKER
+            </StyledLink>
+            {'   '}
+            <StyledLink href='http://bit.ly/MetaFestMG'>METAFAM</StyledLink>
+          </div>
+          <img
+            src={Wanderer}
+            alt=''
+            width='50%'
+            style={{
+              position: 'absolute',
+              bottom: '0%',
+              left: '0%',
+              zIndex: '10'
+            }}
+          />
+          <img
+            src={Rocks}
+            alt=''
+            width='100%'
+            style={{
+              position: 'fixed',
+              marginLeft: 'auto',
+              marginRight: 'auto',
+              left: '0',
+              right: '0',
+              bottom: '0',
+              zIndex: '1'
+            }}
+          />
+          <img
+            src={Grid}
+            alt=''
+            width='100%'
+            height='60%'
+            style={{
+              position: 'fixed',
+              bottom: '0',
+              zIndex: '0'
+            }}
+          />
+        </div>
+      )}
+    </React.Fragment>
   )
 }
 
