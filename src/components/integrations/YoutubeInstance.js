025755af69ccacbb6fc2aa9f6dba8f7f2c27feed
geleeroyale <krassvs@gmail.com>
2021-03-16 14:38:44 +0100

    update youtube component with stream for community day 2

--- src/components/integrations/YoutubeInstance.js
+++ src/components/integrations/YoutubeInstance.js
@@ -2,15 +2,15 @@ import React, { useState, useEffect, useContext } from 'react'
 import { FloatingSpaceContext } from '../../contexts/FloatingSpaceContext'
 
 function YoutubeInstance ({ roomData }) {
-  const [youtubeRoom, setYoutubeRoom] = useState('Euqu6tuAbT4')
+  const [youtubeRoom, setYoutubeRoom] = useState('BnO-3u64fO8')
   const { currentFloatingSpaces } = useContext(FloatingSpaceContext)
   const space = currentFloatingSpaces
 
   useEffect(() => {
     if (space.indexOf('stage') > -1) {
-      setYoutubeRoom('Euqu6tuAbT4')
+      setYoutubeRoom('BnO-3u64fO8')
     } else {
-      setYoutubeRoom('Euqu6tuAbT4')
+      setYoutubeRoom('BnO-3u64fO8')
     }
     return console.log(youtubeRoom)
   }, [space, youtubeRoom])
